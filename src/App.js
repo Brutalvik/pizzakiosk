d60990ec00fb9019def7c2dededcb3f5dcd8a852
Vikram Kumar <vikram@Vikrams-MacBook-Pro.local>
2021-05-20 00:49:27 -0400

    First Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Styles.css'
+
+const Menu = () => {
+    return 'Menu'
+}
+
+const Sidebar = () => {
+    return 'Sidebar'
+}
+
+
+const App = () => 
+    <div>
+        <Menu />
+        <Sidebar />
+    </div>
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './Styles.css'
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>React App</h1>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
